e1981008d349388587ba2fc3437362fa5e55c78b
Ragnar Kaldhand <mandrabura2001@gmail.com>
2024-11-10 21:42:42 +0400

    first commit

---
+++ Pages/PageClasses.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Pages;
+
+public class LoginPage
+{
+    private IWebDriver driver;
+    
+    public LoginPage(IWebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    private By login_field = By.Id("user-name");
+    private By password_field = By.Id("password");
+    private By submit_button = By.Id("login-button");
+
+    public void TextInput(string login, string password)
+    {
+        
+    }
+}
