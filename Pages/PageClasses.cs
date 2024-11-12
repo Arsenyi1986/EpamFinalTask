70d44f939dadafb341c346458b99e5049468a1aa
Ragnar Kaldhand <mandrabura2001@gmail.com>
2024-11-12 23:05:44 +0400

    - Implement error handling

--- Pages/PageClasses.cs
+++ Pages/PageClasses.cs
@@ -15,11 +15,33 @@ public class LoginPage
     {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        try
+        {
+            ValidateElements();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 
-    private By login_field = By.CssSelector("#user-name");
-    private By password_field = By.CssSelector("#password");
-    private By submit_button = By.CssSelector("#login-button");
+    private By login_field;
+    private By password_field;
+    private By submit_button;
+
+    public void ValidateElements()
+    {
+        try
+        {
+            login_field = By.CssSelector("#user-name");
+            password_field = By.CssSelector("#password");
+            submit_button = By.CssSelector("#login-button");
+        }
+        catch (NoSuchElementException)
+        {
+            throw new ArgumentException("Some necessary WebElements were not found.");
+        }
+    }
 
     public void TextInput(string login, string password)
     {
@@ -47,20 +69,47 @@ public class LoginPage
 
     public void Submit()
     {
-        driver.FindElement(submit_button).Click();
+        try
+        {
+            driver.FindElement(submit_button).Click();
+        }
+        catch (NoSuchElementException)
+        {
+            Console.WriteLine("The 'Submit button' element was not found.");
+        }
+        catch (ElementNotInteractableException)
+        {
+            Console.WriteLine("The 'Submit button' element is not interactable");
+        }
     }
 
     public string ReturnErrorInfo()
     {
-        IWebElement error_box = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".error-message-container")));
+        try
+        {
+            IWebElement error_box = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".error-message-container")));
 
-        return error_box.FindElement(By.TagName("h3")).Text;
+            return error_box.FindElement(By.TagName("h3")).Text;
+        }
+        catch (TimeoutException)
+        {
+            Console.WriteLine("Error message container was not found in expected amount of time.");
+            return null;
+        }
     }
 
     public string ReturnDash()
     {
-        IWebElement dash_logo = wait.Until(ExpectedConditions.ElementExists(By.CssSelector(".app_logo")));
+        try
+        {
+            IWebElement dash_logo = wait.Until(ExpectedConditions.ElementExists(By.CssSelector(".app_logo")));
 
-        return dash_logo.Text;
+            return dash_logo.Text;
+        }
+        catch (TimeoutException)
+        {
+            Console.WriteLine("The dashboard message was not found in expected amount of time.");
+            return null;
+        }
     }
 }
--- Pages/obj/Debug/net8.0/Pages.AssemblyInfo.cs
+++ Pages/obj/Debug/net8.0/Pages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+750ea243907cc5ec6a79b2c3aa8bcbf04905fc2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1f805f4ce3d8c6f2b683cd3aa8c31428bde7b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
